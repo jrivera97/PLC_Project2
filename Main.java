43f27de9dcdcb4402d66056519f5bfca0aa8b3c3
Alexandra Isaly <alexandraisaly@gmail.com>
2019-03-17 13:28:41 -0400

    Main file to run code

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.lang.Exception;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length == 0) {
+            args = new String[]{"src/main/mu/test.mu"};
+        }
+
+        System.out.println("parsing: " + args[0]);
+
+        CharStream charStream = CharStreams.fromFileName(args[0]);
+        CalculatorLexer lexer = new CalculatorLexer(charStream);
+        CalculatorParser parser = new CalculatorParser(new CommonTokenStream(lexer));
+        ParseTree tree = parser.prog();
+        EvalVisitor visitor = new EvalVisitor();
+        visitor.visit(tree);
+    }
+}
